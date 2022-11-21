7c9b483238607941f23d92ee538f9a0545d95d16
markkong30 <markkong30@gmail.com>
2022-11-21 00:29:17 +0000

    webpack setup

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './AboutPage.scss';
+
+const AboutPage = () => (
+	<div>
+		<h2>About</h2>
+		<p>This app uses React, Redux, React Router, and many other helpful libraries.</p>
+	</div>
+);
+
+export default AboutPage;
