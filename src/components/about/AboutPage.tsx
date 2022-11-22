ce5be22c9a218b288efc8c71baa49e352412691f
markkong30 <markkong30@gmail.com>
2022-11-22 00:19:37 +0000

    migrate js to ts

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './AboutPage.scss';
 
-const AboutPage = () => (
+const AboutPage: React.FC = () => (
 	<div>
 		<h2>About</h2>
 		<p>This app uses React, Redux, React Router, and many other helpful libraries.</p>
