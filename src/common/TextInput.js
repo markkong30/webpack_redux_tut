fea965a5c2d4fd8895c8399c4c90b1534fdf58e8
markkong30 <markkong30@gmail.com>
2022-12-04 01:24:38 +0000

    remove propTypes

--- src/common/TextInput.js
+++ src/common/TextInput.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const TextInput = ({ name, label, onChange, placeholder, value, error }) => {
 	let wrapperClass = 'form-group';
@@ -23,15 +22,6 @@ const TextInput = ({ name, label, onChange, placeholder, value, error }) => {
 			</div>
 		</div>
 	);
-};
-
-TextInput.propTypes = {
-	name: PropTypes.string.isRequired,
-	label: PropTypes.string.isRequired,
-	onChange: PropTypes.func.isRequired,
-	placeholder: PropTypes.string,
-	value: PropTypes.string,
-	error: PropTypes.string
 };
 
 export default TextInput;
